98f87b9d51fb216770fd5f029ebb0b3b2b6acfa5
Luka Catela <luca.catela8@gmail.com>
2025-04-13 16:27:44 +0200

    vj_2_final
    gotova vjezba 2

--- vjezba-2/src/App.vue
+++ vjezba-2/src/App.vue
@@ -37,32 +37,34 @@ function dohvatiCijenu(naziv){
   return proizvod ? proizvod.cijena : 0
 }
 
-function sveukupnaCijena() {
-  let ukupno = 0;
-  for (const item of korisnik.value.kosarica) {
-    const proizvod = proizvodi.value.find(p => p.naziv === item.naziv);
-    if (proizvod) {
-      ukupno += proizvod.cijena * item.količina;
-    }
-  }
-  return ukupno.toFixed(2);
+function ukupnaCijena(naziv) {
+  const cijena = dohvatiCijenu(naziv);
+  const stavka = korisnik.value.kosarica.find(k => k.naziv === naziv);
+  return stavka ? (cijena * stavka.količina).toFixed(2) : "0.00";
 }
 
-function najskupljaStavka(){
+
+function najskupljaStavka() {
   let najskuplja = null;
   let max = 0;
   for (const item of korisnik.value.kosarica) {
-    const proizvod = proizvodi.value.find(p => p.naziv === item.naziv);
-    if (proizvod) {
-      const ukupnacijena = proizvod.cijena * item.količina;
-      if(ukupnacijena > max){
-        max = ukupnacijena;
-        najskuplja = item.naziv;
-      }
+    const cijena = dohvatiCijenu(item.naziv);
+    const ukupno = cijena * item.količina;
+    if (ukupno > max) {
+      max = ukupno;
+      najskuplja = item.naziv;
     }
   }
   return najskuplja;
 }
+
+const sveukupnaCijena = computed(() =>
+  korisnik.value.kosarica
+    .reduce((ukupno, item) => ukupno + dohvatiCijenu(item.naziv) * item.količina, 0)
+    .toFixed(2)
+);
+
+
 const punoIme = computed(()=> `${korisnik.value.osobni_podaci.ime} ${korisnik.value.osobni_podaci.prezime}`) //Tu sam koristio drugu notaciju zapisa varijable
 const adresa_korisnik = computed(()=> korisnik.value.osobni_podaci.adresa.ulica + korisnik.value.osobni_podaci.adresa.broj + "," + korisnik.value.osobni_podaci.adresa.grad)
 const telefon_korisnik = computed(()=> korisnik.value.osobni_podaci.broj_telefona);
@@ -71,7 +73,7 @@ const telefon_korisnik = computed(()=> korisnik.value.osobni_podaci.broj_telefon
 
 <template>
   <div class="flex flex-col justify-center items-center h-screen">
-    <div class="bg-blue-50 w-1/4 h-auto rounded-sm p-2 text-left"
+    <div class="bg-blue-50 w-2/6 h-auto rounded-sm p-2 text-left"
       :class="korisnik.jeAdmin ? 'text-blue-600' : 'text-black'">
       <span><b>Korisnički podaci</b></span>
       <hr>
@@ -79,12 +81,62 @@ const telefon_korisnik = computed(()=> korisnik.value.osobni_podaci.broj_telefon
       <span><b>Adresa: </b>{{ adresa_korisnik }}</span> <br>
       <span><b>Telefon: </b>{{ telefon_korisnik }}</span>
     </div>
-      <div class="bg-blue-50 w-1/4 h-auto rounded-sm p-2 text-left mt-4 flex flex-col items-center shadow">
+      <div class="bg-blue-50 w-2/6 h-auto rounded-sm p-2 text-left mt-4 flex flex-col items-center shadow">
       <span class="text-2xl"><b>Košarica</b></span><br>
-        <div class="mb-2 p-2 rounded"
-        :class="korisnik.kosarica[0].naziv === najskupljaStavka() ? 'bg-red-200' : 'bg-gray-100'">
-          <img :src="slike[korisnik.kosarica[0].naziv]" class="w-8 inline-block">
+
+        <!--JABUKA-->
+        <div class="my-3 border p-2 rounded w-6/7"
+            :class="korisnik.kosarica[0].naziv === najskupljaStavka() ? 'bg-red-200' : 'bg-gray-50'">
+          <div class="flex items-center gap-4">
+          <img :src="slike[korisnik.kosarica[0].naziv]" class="w-12 h-12 object-contain">
+          <div class="flex flex-col">
+            <b class="text-lg">{{ korisnik.kosarica[0].naziv }}</b>
+            <span>Cijena: €{{ dohvatiCijenu("Jabuka") }} | Količina: {{ korisnik.kosarica[0].količina }}</span>
+            <span>Ukupno: €{{ ukupnaCijena("Jabuka") }}</span>
+          </div>
+          </div>
         </div>
+        <!-- MRKVA -->
+        <div class="my-3 border p-2 rounded w-6/7"
+            :class="korisnik.kosarica[1].naziv === najskupljaStavka() ? 'bg-red-200' : 'bg-gray-50'">
+          <div class="flex items-center gap-4">
+            <img :src="slike[korisnik.kosarica[1].naziv]" class="w-12 h-12 object-contain">
+            <div class="flex flex-col">
+              <b class="text-lg">{{ korisnik.kosarica[1].naziv }}</b>
+              <span>Cijena: €{{ dohvatiCijenu('Mrkva') }} | Količina: {{ korisnik.kosarica[1].količina }}</span>
+              <span>Ukupno: €{{ ukupnaCijena('Mrkva') }}</span>
+            </div>
+          </div>
+        </div>
+
+        <!-- KRUH -->
+        <div class="my-3 border p-2 rounded w-6/7"
+            :class="korisnik.kosarica[3].naziv === najskupljaStavka() ? 'bg-red-200' : 'bg-gray-50'">
+          <div class="flex items-center gap-4">
+            <img :src="slike[korisnik.kosarica[3].naziv]" class="w-12 h-12 object-contain">
+            <div class="flex flex-col">
+              <b class="text-lg">{{ korisnik.kosarica[3].naziv }}</b>
+              <span>Cijena: €{{ dohvatiCijenu('Kruh') }} | Količina: {{ korisnik.kosarica[3].količina }}</span>
+              <span>Ukupno: €{{ ukupnaCijena('Kruh') }}</span>
+            </div>
+          </div>
+        </div>
+
+        <!-- SIR -->
+        <div class="my-3 border p-2 rounded w-6/7"
+            :class="korisnik.kosarica[2].naziv === najskupljaStavka() ? 'bg-red-200' : 'bg-gray-50'">
+          <div class="flex items-center gap-4">
+            <img :src="slike[korisnik.kosarica[2].naziv]" class="w-12 h-12 object-contain">
+            <div class="flex flex-col">
+              <b class="text-lg">{{ korisnik.kosarica[2].naziv }}</b>
+              <span>Cijena: €{{ dohvatiCijenu('Sir') }} | Količina: {{ korisnik.kosarica[2].količina }}</span>
+              <span>Ukupno: €{{ ukupnaCijena('Sir') }}</span>
+            </div>
+          </div>
+        </div>
+
+        <br>
+        <span><b>Ukupna cijena:</b>€ {{ sveukupnaCijena }}</span>
       </div>
   </div>
 </template>
